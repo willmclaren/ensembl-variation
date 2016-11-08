3d3acc5648f2dde671b0f72203e8e39a4802d961
William McLaren <wm2@ebi.ac.uk>
2016-11-08 15:01:28 +0000

    add check_dirs and run_test_set routines

--- modules/Bio/EnsEMBL/Variation/Pipeline/DumpVEP/QCDump.pm
+++ modules/Bio/EnsEMBL/Variation/Pipeline/DumpVEP/QCDump.pm
@@ -36,9 +36,17 @@ use base qw(Bio::EnsEMBL::Variation::Pipeline::DumpVEP::BaseVEP);
 
 use File::Path qw(make_path rmtree);
 use Digest::MD5 qw(md5_hex);
+use JSON;
+use FileHandle;
+use Data::Dumper;
+$Data::Dumper::Indent = 1;
 
 use Bio::EnsEMBL::VEP::Config;
 use Bio::EnsEMBL::VEP::CacheDir;
+use Bio::EnsEMBL::VEP::Runner;
+use Bio::EnsEMBL::Registry;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
 
 sub param_defaults {
   return {
@@ -54,6 +62,8 @@ sub run {
 
   $self->qc();
   $self->qc('_tabixconverted') if $self->param('convert');
+
+  return 1;
 }
 
 sub qc {
@@ -66,16 +76,16 @@ sub qc {
   my $species   = $self->required_param('species');
   my $assembly  = $self->required_param('assembly');
 
+  my $dump_dir = $self->dump_dir();
   my $tar_file = $self->get_tar_file_name(
     $dump_dir,
-    $species,
+    $species.($type eq 'core' ? '' : "_$type"),
     $assembly,
     $mod
   );
 
   die("ERROR: Tar file $tar_file not found\n") unless -e $tar_file;
 
-  my $dump_dir = $self->dump_dir();
   my $qc_dir = $dump_dir.'/qc/'.md5_hex($self->input_id);
   rmtree($qc_dir) if -d $qc_dir;
   make_path($qc_dir);
@@ -92,13 +102,27 @@ sub qc {
   die("ERROR: Expected to find $extracted_dir/info.txt") unless -e "$extracted_dir/info.txt";
 
   # create objects
-  my $config_obj = Bio::EnsEMBL::VEP::Config->new({dir => $extracted_dir, offline => 1, species => $species});
+  my $config_obj = Bio::EnsEMBL::VEP::Config->new({
+    dir => $extracted_dir,
+    offline => 1,
+    species => $species,
+    assembly => $assembly,
+    check_existing => 1,
+    regulatory => 1,
+  });
   my $cache_dir_obj = Bio::EnsEMBL::VEP::CacheDir->new({dir => $extracted_dir, config => $config_obj});
 
   # these subs check different aspects of the cache
   $self->check_info($cache_dir_obj, $converted);
 
   $self->check_annotation_sources($cache_dir_obj, $converted);
+
+  $self->check_dirs($cache_dir_obj, $converted);
+
+  $self->run_test_set($qc_dir) if $has_var && $type ne 'otherfeatures';
+
+  # clean up
+  rmtree($qc_dir);
 }
 
 sub check_info {
@@ -132,10 +156,11 @@ sub check_info {
     die("ERROR: variation_cols info key not found\n") unless $info->{variation_cols};
 
     die("ERROR: variation_cols info value looks wrong: ".$info->{variation_cols}."\n")
-      unless $info->{variation_cols} =~ /(\w+\,?)+/;
+      unless ref($info->{variation_cols}) eq 'ARRAY';
 
     if($converted) {
-      die("ERROR: variation_cols value does not include chr\n") unless $info->{variation_cols} =~ /^chr\,/;
+      die("ERROR: variation_cols value does not include chr\n")
+        unless $info->{variation_cols}->[0] eq 'chr';
     }
   }
 }
@@ -160,6 +185,218 @@ sub check_annotation_sources {
     die("ERROR: No Regulation annotation source found\n")
       unless grep {ref($_) eq 'Bio::EnsEMBL::VEP::AnnotationSource::Cache::RegFeat'} @$annotation_sources;
   }
+}
+
+sub check_dirs {
+  my ($self, $cache_dir_obj, $converted) = @_;
+
+  my $dir = $cache_dir_obj->dir;
+  opendir DIR, $dir or die $!;
+  my %dirs_in_cache;
+  $dirs_in_cache{$_} = 1 for grep {!/^\./ && -d "$dir/$_" } readdir(DIR);
+  closedir DIR;
+
+  die("ERROR: No chromosome dirs found\n") unless scalar keys %dirs_in_cache;
+
+  # connect to DB to find all the slices we should have dirs for
+  Bio::EnsEMBL::Registry->clear();
+  my $dba = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+    -group   => 'core',
+    -species => $self->param('species'),
+    -port    => $self->param('port'),
+    -host    => $self->param('host'),
+    -user    => $self->param('user'),
+    -pass    => $self->param('pass'),
+    -dbname  => $self->param('dbname'),
+    -MULTISPECIES_DB => $self->param('is_multispecies'),
+    -species_id => $self->param('species_id'),
+  );
+
+  # get slices
+  my $sa = $dba->get_SliceAdaptor;
+  my @slices = @{$sa->fetch_all('toplevel')};
+  push @slices, map {$_->alternate_slice} map {@{$_->get_all_AssemblyExceptionFeatures}} @slices;
+  push @slices, @{$sa->fetch_all('lrg', undef, 1, undef, 1)} if $self->param('lrg');
+
+  # check for dirs that don't appear in this list of slices
+  delete($dirs_in_cache{$_->seq_region_name}) for @slices;
+  die("ERROR: Found ".(scalar keys %dirs_in_cache)." dirs in cache that have no corresponding slice\n") if scalar keys %dirs_in_cache;
+
+  # now filter out slices with no transcripts
+  my $ta = $dba->get_TranscriptAdaptor();
+  @slices = grep {$ta->count_all_by_Slice($_)} @slices;
+
+  foreach my $slice(@slices) {
+    my $chr = $slice->seq_region_name;
+    die("ERROR: Dir $dir/$chr missing\n") unless -d "$dir/$chr";
+
+    opendir SUB, "$dir/$chr" or die $!;
+    my @files = grep {!/^\./} readdir(SUB);
+    closedir SUB;
+
+    die("ERROR: $dir/$chr is empty\n") unless @files;
+
+    foreach my $file(@files) {
+      die("ERROR: File name $file does not look right\n")
+        unless $file =~ /\d+\-\d+(\_(var|reg))?\.gz/
+        or $file eq 'all_vars.gz'
+        or $file eq 'all_vars.gz.tbi';
+
+      if($file =~ /var/) {
+        die("ERROR: Cache should not contain var files\n") unless $self->param('variation');
+        die("ERROR: Converted cache should not contain per-MB files\n") if $converted && $file =~ /\_var\.gz/;
+        die("ERROR: Basic cache should not contain all_vars.gz file\n") if !$converted && $file eq 'all_vars.gz';
+      }
+      elsif($file =~ /reg/) {
+        die("ERROR: Cache should not contain var files\n") unless $self->param('regulation');
+      }
+    }
+  }
+}
+
+sub run_test_set {
+  my $self = shift;
+  my $qc_dir = shift;
+
+  # get a test file
+  my $test_file = $self->generate_test_file();
+
+  # create a VEP Runner
+  my $runner = Bio::EnsEMBL::VEP::Runner->new({
+    species => $self->param('species'),
+    assembly => $self->param('assembly'),
+    cache_version => $self->param('eg_version') || $self->param('ensembl_release'),
+    offline => 1,
+    database => 0,
+    dir => $qc_dir,
+    input_file => $test_file,
+    format => 'ensembl',
+    output_format => 'json',
+    safe => 1,
+    quiet => 1,
+    no_stats => 1,
+    regulatory => $self->param('regulation'),
+    check_existing => $self->param('variation'),
+  });
+
+  my $json = JSON->new();
+
+  while(my $line = $runner->next_output_line) {
+    my $data = $json->decode($line);
+
+    die("ERROR: input field not found in JSON output\n".(Dumper $data)) unless $data->{input};
+    my @input = split("\t", $data->{input});
+
+    my $feature_type = $input[-1];
+    my $expected_cons = join(",", sort split(",", $input[-2]));
+    my $feature_id = $input[-3];
+
+    # check consequence type
+    die("ERROR: no data for $feature_type found in JSON output\n".(Dumper $data)) unless $data->{$feature_type.'_consequences'};
+
+    if(my ($blob) = grep {$_->{$feature_type.'_id'} eq $feature_id} @{$data->{$feature_type.'_consequences'}}) {
+      die("ERROR: no consequence_terms field in blob\n".(Dumper $data)) unless $blob->{consequence_terms};
+      my $got_cons = join(",", sort @{$blob->{consequence_terms}});
+      die("ERROR: consequence_types don't match, expected: $expected_cons, got: $got_cons\n".(Dumper $data)) unless $expected_cons eq $got_cons;
+    }
+    else {
+      die("ERROR: no data for $feature_id found in JSON output\n".(Dumper $data));
+    }
+
+    # check colocated variants
+    if($self->param('variation')) {
+      my $expected_var_id = $input[-4];
+      die("ERROR: no data for colocated_variants found in JSON output\n".(Dumper $data)) unless $data->{colocated_variants};
+
+      die("ERROR: expected var id $expected_var_id not found\n".(Dumper $data))
+        unless grep {$_->{id} eq $expected_var_id} @{$data->{colocated_variants}};
+    }
+  }
+}
+
+sub generate_test_file {
+  my $self = shift;
+
+  my $file = sprintf(
+    '%s/qc/%s_%s_test_input.txt',
+    $self->dump_dir(),
+    $self->param('species'),
+    $self->param('assembly')
+  );
+
+  return $file if -e $file;
+
+  # use a lock file in case two processes try writing to the same one
+  my $lock = $file.'.lock';
+
+  # wait 3hrs for other process to finish, it will be a long-running query in human
+  my $sleep_count = 0;
+  if(-e $lock) {
+    while(-e $lock) {
+      sleep 1;
+      die("I've been waiting for $lock to be removed for $sleep_count seconds, something may have gone wrong\n") if ++$sleep_count > (60*60*3);
+    }
+
+    die("ERROR: Finished waiting but $file not found\n") unless -e $file;
+
+    return $file;
+  }
+
+  open OUT, ">$lock" or die $!;
+  print OUT "$$\n";
+  close OUT;
+  $self->{_lock_file} = $lock;
+
+  my $dbname = $self->param('dbname');
+  $dbname =~ s/core|otherfeatures/variation/;
+
+  my $dba = Bio::EnsEMBL::Variation::DBSQL::DBAdaptor->new(
+    -group   => 'variation',
+    -species => $self->param('species'),
+    -port    => $self->param('port'),
+    -host    => $self->param('host'),
+    -user    => $self->param('user'),
+    -pass    => $self->param('pass'),
+    -dbname  => $dbname,
+    -MULTISPECIES_DB => $self->param('is_multispecies'),
+    -species_id => $self->param('species_id'),
+  );
+
+  my @tables = ('transcript', ($self->param('regulation') ? qw(regulatory_feature motif_feature) : ()));
+
+  my $fh;
+
+  foreach my $table(@tables) {
+    my $sth = $dba->dbc->prepare(qq{
+      SELECT s.name, vf.seq_region_start, vf.seq_region_end, vf.allele_string, vf.seq_region_strand,
+      vf.variation_name, tv.feature_stable_id, tv.consequence_types
+      FROM seq_region s, $table\_variation tv, variation_feature vf
+      LEFT JOIN failed_variation fv on vf.variation_id = fv.variation_id
+      WHERE s.seq_region_id = vf.seq_region_id
+      AND vf.variation_feature_id = tv.variation_feature_id
+      AND fv.variation_id IS NULL
+      GROUP BY vf.seq_region_id, tv.consequence_types
+    }, {mysql_use_result => 1});
+    $sth->execute;
+
+    $fh ||= FileHandle->new($file, 'r');
+
+    while(my $arrayref = $sth->fetchrow_arrayref()) {
+      print $fh join("\t", @$arrayref, $table)."\n";
+    }
+
+    $sth->finish();
+  }
+
+  $fh->close();
+
+  unlink($lock);
+
+  return $file;
+}
+
+sub DESTROY {
+  unlink($_[0]->{_lock_file}) if $_[0]->{_lock_file};
 }
 
 1;
